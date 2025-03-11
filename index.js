0509c10ebfb124bdfae90ecccdb3eecaa305a048
dennissammy77 <dennissammy77@gmail.com>
2025-03-11 13:36:46 +0300

    array filters

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(drivers, nameSearch){
+    return drivers.filter((name)=>
+        name.toLowerCase() === nameSearch.toLowerCase()
+    )
+}
+
+function fuzzyMatch(drivers, nameSearch){
+    return drivers.filter((name)=>
+        name.toLowerCase()[0] === nameSearch.toLowerCase()[0]
+    )
+}
+function matchName(drivers, nameSearch){
+    return drivers.filter((driver)=>
+        driver.name.toLowerCase() === nameSearch.toLowerCase()
+    )
+}
